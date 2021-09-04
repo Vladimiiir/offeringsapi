f0c062ce623084c48cf6cb8efd70f43c8c9663c0
Vladimir <vladimir.abouassaly@gmail.com>
2021-09-04 11:38:24 +0300

    CORS Added

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const express = require("express");
 const app = express();
+var cors = require('cors')
+app.use(cors())
+
 const importData = require("./data.json")
 let port = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
+app.get("/", (req, res, next) => {
     res.send("Our Offerings: Certificates, Diplomas, Bachelors, Masters");
 })
 
-app.get("/offerings", (req, res) => {
+app.get("/offerings", (req, res, next) => {
     res.send(importData);
 })
 
